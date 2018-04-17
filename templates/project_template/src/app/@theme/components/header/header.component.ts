8e765f2a924bc9699891571ee733cca2652c6541
geiner <geiner.salcedo.ppc0111@gmail.com>
2018-04-17 13:18:24 -0500

    version stable

---
+++ templates/project_template/src/app/@theme/components/header/header.component.ts
@@ -0,0 +1,67 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+import { NbMenuService, NbSidebarService } from '@nebular/theme';
+import { AnalyticsService } from '../../../@core/utils/analytics.service';
+import { AutenticationService } from '../../../@core/utils/autentication.service';
+import { TranslateService } from '@ngx-translate/core';
+
+@Component({
+  selector: 'ngx-header',
+  styleUrls: ['./header.component.scss'],
+  templateUrl: './header.component.html',
+})
+export class HeaderComponent implements OnInit {
+
+  @Input() position = 'normal';
+
+  user: any;
+  userMenu: any;
+  title: any;
+  username = '';
+
+  constructor(private sidebarService: NbSidebarService,
+    private menuService: NbMenuService,
+    private analyticsService: AnalyticsService,
+    private autenticacion: AutenticationService,
+    public translate: TranslateService) {
+    this.translate = translate;
+  }
+  useLanguage(language: string) {
+    this.translate.use(language);
+  }
+  ngOnInit() {      
+    this.autenticacion.init();
+  }
+  liveToken() {
+    if (this.autenticacion.live()) {
+      this.username = (this.autenticacion.getPayload()).sub;
+    }
+    return this.autenticacion.live();
+  }
+
+  login() {
+    location.href = this.autenticacion.getAuthorizationUrl();
+  }
+
+  logout() {
+    location.href = this.autenticacion.getLogoutUrl();
+  }
+
+  toggleSidebar(): boolean {
+    this.sidebarService.toggle(true, 'menu-sidebar');
+    return false;
+  }
+
+  toggleSettings(): boolean {
+    this.sidebarService.toggle(false, 'settings-sidebar');
+    return false;
+  }
+
+  goToHome() {
+    this.menuService.navigateHome();
+  }
+
+  startSearch() {
+    this.analyticsService.trackEvent('startSearch');
+  }
+}
---
+++ templates/project_template/src/app/@theme/theme.module.ts
@@ -0,0 +1,119 @@
+import { ModuleWithProviders, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AutenticationService } from '../@core/utils/autentication.service';
+import { SharedModule } from '../shared/shared.module';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material';
+import { MatInputModule } from '@angular/material';
+
+import {
+  NbActionsModule,
+  NbCardModule,
+  NbLayoutModule,
+  NbMenuModule,
+  NbRouteTabsetModule,
+  NbSearchModule,
+  NbSidebarModule,
+  NbTabsetModule,
+  NbThemeModule,
+  NbUserModule,
+  NbCheckboxModule,
+} from '@nebular/theme';
+
+import {
+  FooterComponent,
+  HeaderComponent,
+  SearchInputComponent,
+  ThemeSettingsComponent,
+  ThemeSwitcherComponent,
+  TinyMCEComponent,
+  DinamicformComponent,
+  SelectComponent,
+} from './components';
+
+import { CapitalizePipe, PluralPipe, RoundPipe, TimingPipe } from './pipes';
+
+import {
+  OneColumnLayoutComponent,
+  SampleLayoutComponent,
+  ThreeColumnsLayoutComponent,
+  TwoColumnsLayoutComponent,
+} from './layouts';
+
+import { DEFAULT_THEME } from './styles/theme.default';
+import { COSMIC_THEME } from './styles/theme.cosmic';
+import { MatNativeDateModule } from '@angular/material/core';
+
+const BASE_MODULES = [CommonModule, FormsModule, ReactiveFormsModule];
+
+const NB_MODULES = [
+  NbCardModule,
+  NbLayoutModule,
+  NbTabsetModule,
+  NbRouteTabsetModule,
+  NbMenuModule,
+  NbUserModule,
+  NbActionsModule,
+  NbSearchModule,
+  NbSidebarModule,
+  NbCheckboxModule,
+  NgbModule,
+];
+
+const MAT_MODULES = [
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatFormFieldModule,
+  MatInputModule,
+]
+
+const COMPONENTS = [
+  ThemeSwitcherComponent,
+  HeaderComponent,
+  FooterComponent,
+  SearchInputComponent,
+  ThemeSettingsComponent,
+  TinyMCEComponent,
+  OneColumnLayoutComponent,
+  SampleLayoutComponent,
+  ThreeColumnsLayoutComponent,
+  TwoColumnsLayoutComponent,
+  DinamicformComponent,
+  SelectComponent,
+];
+
+const PIPES = [
+  CapitalizePipe,
+  PluralPipe,
+  RoundPipe,
+  TimingPipe,
+];
+
+const NB_THEME_PROVIDERS = [
+  ...NbThemeModule.forRoot(
+    {
+      name: 'default',
+    },
+    [DEFAULT_THEME, COSMIC_THEME],
+  ).providers,
+  ...NbSidebarModule.forRoot().providers,
+  ...NbMenuModule.forRoot().providers,
+];
+
+
+@NgModule({
+  imports: [...BASE_MODULES, ...NB_MODULES, SharedModule, ...MAT_MODULES],
+  exports: [...BASE_MODULES, ...NB_MODULES, ...COMPONENTS, ...PIPES],
+  declarations: [...COMPONENTS, ...PIPES, DinamicformComponent, SelectComponent],
+})
+export class ThemeModule {
+  static forRoot(): ModuleWithProviders {
+    return <ModuleWithProviders>{
+      ngModule: ThemeModule,
+      providers: [...NB_THEME_PROVIDERS,
+        AutenticationService],
+    };
+  }
+}
